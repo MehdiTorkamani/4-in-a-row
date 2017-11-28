59e038ae1bda79b79c208a17280ad07cf4880816
MehdiTorkamani <33295235+MehdiTorkamani@users.noreply.github.com>
2017-11-28 13:52:38 +0000

    Filesize reduced, code and art polished
    Redundant assets deleted.
    Seperate class made for UIManager to handle different phases/UI interactions of the game.
    Improvement to the art of the game.

---
+++ 4 in a row/Assets/Scripts/ColorChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+
+	float a;
+	float x = 1;
+	bool goUp = true;
+	Color test = new Color ();
+
+	void Start () {
+		test = gameObject.GetComponent<SpriteRenderer> ().color;
+	}
+
+	//Color change could also be achieved with Coroutine
+	void Update () {
+		test.a = (80-x) / 80;
+		gameObject.GetComponent<SpriteRenderer> ().color = test;
+
+		if (x <= 0)
+			goUp = true;
+		else if (x >= 80)
+			goUp = false;
+
+		if (goUp)
+			x++;
+		else
+			x--;
+	}
+}
---
+++ 4 in a row/Assets/Scripts/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+	public static UIManager Instance;
+
+	[SerializeField] GameObject startPanel;
+	[SerializeField] GameObject endPanel;
+	[SerializeField] GameObject rIcon;
+	[SerializeField] GameObject yIcon;
+
+	void Awake()
+	{
+		if (Instance == null)
+			Instance = this;
+		else
+			Destroy (this);
+	}
+
+	public void BeginGame(int a)
+	{
+		startPanel.SetActive (false);
+		GameController.Instance.turn = a;
+		GameController.Instance.currentBall = Instantiate (GameController.Instance.turn == 1 ? GameController.Instance.rBall : GameController.Instance.yBall);
+		GameController.Instance.currentBall.GetComponent<Rigidbody2D> ().isKinematic = true;
+		GameController.Instance.gameStarted = true;
+	}
+
+	public IEnumerator GameEnded (int a)
+	{
+		yield return new WaitForSeconds (2f);
+
+		endPanel.SetActive (true);
+		if (a == 1)
+			rIcon.SetActive (true);
+		else
+			yIcon.SetActive (true);
+	}
+
+	public void RestartGame()
+	{
+		SceneManager.LoadScene (0);
+	}
+}
